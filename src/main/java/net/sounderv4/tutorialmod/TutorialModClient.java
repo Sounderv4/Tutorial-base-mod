8c83da78bfe85e90765d019e15d3360c8927882e
Sounderv4 <blackwellcayden@gmail.com>
2025-06-20 01:09:19 -0400

    person

---
+++ src/main/java/net/sounderv4/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package net.sounderv4.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sounderv4/tutorialmod/TutorialmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sounderv4.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
