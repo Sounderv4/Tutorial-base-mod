7114e0d88413e63c13a23a95a3d44ca306d8c835
Sounderv4 <blackwellcayden@gmail.com>
2025-08-02 10:13:09 -0400

    Mod Name Change

--- src/main/java/net/sounderv4/tutorialmod/TutorialModClient.java
+++
@@ -1,9 +0,0 @@
-package net.sounderv4.tutorialmod;
-import net.fabricmc.api.ClientModInitializer;
-
-public class TutorialModClient implements ClientModInitializer{
-    @Override
-    public void onInitializeClient() {
-
-    }
-}
